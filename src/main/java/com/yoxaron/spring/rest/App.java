d0a92437f14ef186a24d7f26820b5691a75889f2
yoxaron <andanotheronemailbox@gmail.com>
2023-08-13 02:58:23 +0100

    add crud operations

--- src/main/java/com/yoxaron/spring/rest/App.java
+++ src/main/java/com/yoxaron/spring/rest/App.java
@@ -1,13 +1,36 @@
 package com.yoxaron.spring.rest;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.yoxaron.spring.rest.configuration.MyConfig;
+import com.yoxaron.spring.rest.entity.Employee;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
+public class App {
+    public static void main( String[] args ) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(MyConfig.class);
+
+        Communication communication = context.getBean("communication", Communication.class);
+
+        //get all
+        List<Employee> allEmployees = communication.getAllEmployees();
+        System.out.println(allEmployees);
+
+        //get by ID
+        Employee empById = communication.getEmployee(2);
+        System.out.println(empById);
+
+        //add new employee
+        Employee emp = new Employee("Ivan", "Sidorov", "HR", 500);
+        communication.saveEmployee(emp);
+
+        //update employee
+        emp = new Employee("Ivan", "Sidorov", "IT", 900);
+        emp.setId(9);
+        communication.saveEmployee(emp);
+
+        //delete employee
+        communication.deleteEmployee(9);
     }
 }
---
+++ src/main/java/com/yoxaron/spring/rest/Communication.java
@@ -0,0 +1,50 @@
+package com.yoxaron.spring.rest;
+
+import com.yoxaron.spring.rest.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.sql.SQLOutput;
+import java.util.List;
+
+@Component
+public class Communication {
+    @Autowired
+    private RestTemplate restTemplate;
+    private final String URL = "http://localhost:8080/spring_rest/api/employees/";
+
+    public List<Employee> getAllEmployees() {
+        ResponseEntity<List<Employee>> responseEntity =
+                restTemplate.exchange(URL, HttpMethod.GET, null
+                        , new ParameterizedTypeReference<List<Employee>>() {});
+        return responseEntity.getBody();
+    }
+
+    public Employee getEmployee(int id) {
+        Employee employee = restTemplate.getForObject(URL + "/" + id, Employee.class);
+        return employee;
+    }
+
+    public void saveEmployee(Employee employee) {
+        int id = employee.getId();
+
+        if (id == 0) {
+            ResponseEntity<String> responseEntity =
+                    restTemplate.postForEntity(URL, employee, String.class);
+            System.out.println("A new worker has been added to the database!");
+            System.out.println(responseEntity.getBody());
+        } else {
+            restTemplate.put(URL, employee);
+            System.out.println("Employee with ID=" + id + " has been updated!");
+        }
+    }
+
+    public void deleteEmployee(int id) {
+        restTemplate.delete(URL + "/" + id);
+        System.out.println("Employee with ID=" + id + " has been deleted!");
+    }
+}
